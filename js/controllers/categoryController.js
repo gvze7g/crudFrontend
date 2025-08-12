134bcb6c617070a0c47a5cbab8258aa91199bfaa
gvze7g <20230315@ricaldone.edu.sv>
2025-08-12 15:16:48 -0600

    commit

--- js/controllers/categoryController.js
+++ js/controllers/categoryController.js
@@ -4,29 +4,117 @@ import {
     updateCategory,
     deleteCategory
 } from "../services/categoryService.js";
-
+ 
 document.addEventListener("DOMContentLoaded", ()=>{
     const tableBody = document.querySelector("#categoriesTable tbody");
     const form = document.getElementById("categoryForm");
-    const modal = new boostrap.Modal(document.getElementById("categoryModal"));
+    const modal = new bootstrap.Modal(document.getElementById("categoryModal"));
     const lblModal = document.getElementById("categoryModalLabel");
-    const btnAdd = document.getElementById("btnAddCategory");
-
-    Infinity();
-
+    const btnAdd = document.getElementById("btnAddCategory"); //Botón para abrir modal
+ 
+    init(); //Este método permite cargar las categorías en la tabla
+ 
     btnAdd.addEventListener("click", ()=>{
         form.reset();
         form.categoryId.value = "";
-        lblModal.textContent = "Agregar categoría";
+        lblModal.textContent = "Agregar Categoria";
         modal.show();
     });
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const id = form.categoryId.value;
+ 
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault(); //Evitamos que el formulario se envie al hacer "submit"
+        const id = form.categoryId.value; //Obtenemos el ID del registro
         const data = {
-          nombreCategoria: form.categoryName.value.trim(),
-          descripcion: form.categoryDescription.value.trim()  
+            nombreCategoria : form.categoryName.value.trim(),
+            descripcion : form.categoryDescription.value.trim()
         };
+ 
+        try{
+            if(id){
+                await updateCategory(id, data);
+            }
+            //Si no hay un ID, significa que estamos agregando
+            else{
+                await createCategory(data);
+            }
+            modal.hide(); //Se oculta el formulario despues de agregar/actualizar
+            await loadCategories(); //Nos permite cargar las categorías
+        }
+        catch(err){
+            console.error("Error: ", err);
+        }
     });
-});
+ 
+ 
+    async function loadCategories() {
+       
+        try{
+            const categories = await getCategories();
+            tableBody.innerHTML = ""; //Vaciamos la tabla
+ 
+            //Verificamos si NO hay categorías
+            if(!categories || categories.length == 0){
+                tableBody.innerHTML = '<td colspan="5">Actualmente no hay registros</td>';
+                return; //Evitamos que se ejecute el resto del codigo
+            }
+            categories.forEach((cat)=>{
+                const tr = document.createElement("tr"); //Se crea el elemento con JS
+                tr.innerHTML = `
+                    <td>${cat.idCategoria}</td>
+                    <td>${cat.nombreCategoria}</td>
+                    <td>${cat.descripcion || ""}</td>
+                    <td>${cat.fechaCreacion || ""}</td>
+                    <td>
+                        <button class="btn btn-sm btn-outline-secondary edit-btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-square-pen">
+                <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/>
+              </svg></button>
+                       
+             
+              <button class="btn btn-sm btn-outline-danger delete-btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-trash">
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+                <path d="M3 6h18"/>
+                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+              </svg>
+ 
+              </button>
+              </td>
+                `;
+ 
+                //Funcionalidad para botones de Editar
+                tr.querySelector(".edit-btn").addEventListener("click", ()=>{
+                    form.categoryId.value = cat.idCategoria;
+                    form.categoryName.value = cat.nombreCategoria;
+                    form.categoryDescription.value = cat.descripcion;
+                    lblModal.textContent = "Editar categoría";
+                   
+                    //El modal se carga hasta que el formulario ya tenga los datos cargados
+                    modal.show();
+                });
+ 
+                //Funcionalidad para botones de Eliminar
+                tr.querySelector(".delete-btn").addEventListener("click", ()=>{
+                    if(confirm("¿Desea eliminar esta categoría?")){
+                        deleteCategory(cat.idCategoria).then(loadCategories);
+                    }
+                });
+ 
+                tableBody.appendChild(tr); //Al TBODY se le concatena la nueva fila creada
+            });
+        }
+        catch(err){
+            console.error("Error cargando categorias: ", err);
+        }
+    }
+ 
+    function init(){
+        loadCategories();
+    }
+});//Esto no se toca
--- js/services/categoryService.js
+++ js/services/categoryService.js
@@ -1,28 +1,28 @@
 const API_URL = "http://localhost:8080/api/category";
-
-async function getCategories(){
+ 
+export async function getCategories(){
     const res = await fetch(`${API_URL}/getDataCategories`);
     return res.json();
 }
-
-async function createCategory(data){
+ 
+export async function createCategory(data) {//"data" son los datos que se guardan
     await fetch(`${API_URL}/newCategory`, {
-        method: "POST",
-        headers: {"Content_Type":"application/json"},
-        body: JSON.stringify(data)
+        method : "POST",
+        headers : {"Content-Type":"application/json"},
+        body : JSON.stringify(data)
     });
 }
-
-async function updateCategory(id, data){
+ 
+export async function updateCategory(id, data) {
     await fetch(`${API_URL}/updateCategory/${id}`, {
-        method:"PUT",
-        headers:{"Content_Type" : "application/json"},
-        body: JSON.stringify(data)
+        method : "PUT",
+        headers : {"Content-Type":"application/json"},
+        body : JSON.stringify(data)
     });
 }
-
-async function deleteCategory(id){
-    await fetch(`${API_URL}/deleteCategory/${id}`, {
-        method:"DELETE"
-    });
+ 
+export async function deleteCategory(id) {
+    await fetch(`${API_URL}/deleteCategory/${id}`,{
+        method : "DELETE"        
+    })
 }
